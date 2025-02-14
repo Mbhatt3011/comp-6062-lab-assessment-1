17b310df52694e044627415afc36fa6d1499f819
Mbhatt3011 <bhattmaharshi09@gmail.com>
2025-02-13 19:46:06 -0500

    Lab Assessment 1

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+console.log('JS loaded!!');
+
+const studentFullName = 'Maharshi Bhatt'
+const studentNumber = 1162741;
+const result = `Maharshi Bhatt - ${studentNumber}`;
+
+
+console.log(result);
+
+let headerContent = document.querySelector("h1");
+headerContent.classList.add("h1");
+headerContent.textContent = result;
+
+
+
+
+
